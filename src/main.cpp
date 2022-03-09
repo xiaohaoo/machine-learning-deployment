86b210ddb9589e47f608b46bac45af683169f5cb
小浩 <sdwenhappy@163.com>
2022-03-09 23:44:22 +0800

    feat(onnx build):

--- src/main.cpp
+++ src/main.cpp
@@ -13,73 +13,70 @@
 #include <algorithm>
 #include <unistd.h>
 
-
-#define constexpr const
-
 using namespace std;
 
-int main(){
+int main() {
 
-    clock_t start_time = clock();
-    
-    // 创建启动环境
-    Ort::Env env(ORT_LOGGING_LEVEL_WARNING, "ONNX");
+	clock_t start_time = clock();
 
-    // 优化设置
-    Ort::SessionOptions session_options;
-    session_options.SetIntraOpNumThreads(int(sysconf(_SC_NPROCESSORS_ONLN)));
-    session_options.SetGraphOptimizationLevel(GraphOptimizationLevel::ORT_ENABLE_EXTENDED);
+	// 创建启动环境
+	Ort::Env env(ORT_LOGGING_LEVEL_WARNING, "ONNX");
 
-    // 内存分配
-    Ort::AllocatorWithDefaultOptions allocator;
+	// 优化设置
+	Ort::SessionOptions session_options;
+	session_options.SetIntraOpNumThreads(int(sysconf(_SC_NPROCESSORS_ONLN)));
+	session_options.SetGraphOptimizationLevel(GraphOptimizationLevel::ORT_ENABLE_EXTENDED);
 
-    const char *model_path = "onnx.onnx";
+	// 内存分配
+	Ort::AllocatorWithDefaultOptions allocator;
 
-    Ort::Session session(env, model_path, session_options);
+	const string model_path = "onnx.onnx";
 
-    // 输入节点
-    const char *modal_input_names[] = {session.GetInputName(0, allocator)};
-    // 输出节点
-    const char *modal_output_names[] = {session.GetOutputName(0, allocator)};
+	Ort::Session session(env, model_path.data(), session_options);
 
-    Ort::MemoryInfo memory_info = Ort::MemoryInfo::CreateCpu(OrtArenaAllocator, OrtMemTypeDefault);
+	// 输入节点
+	const char* modal_input_names[] = { session.GetInputName(0, allocator) };
+	// 输出节点
+	const char* modal_output_names[] = { session.GetOutputName(0, allocator) };
 
-    // 模型输入和输出形状
-    // 批 × 长 × 宽 × 通道
-    std::vector<int64_t> modal_input_shape = session.GetInputTypeInfo(0).GetTensorTypeAndShapeInfo().GetShape();
-    std::vector<int64_t> modal_output_shape = session.GetOutputTypeInfo(0).GetTensorTypeAndShapeInfo().GetShape();
+	Ort::MemoryInfo memory_info = Ort::MemoryInfo::CreateCpu(OrtArenaAllocator, OrtMemTypeDefault);
 
-    printf("-> 模型输入形态：%ld × %ld × %ld × %ld\n", modal_input_shape[0], modal_input_shape[1], modal_input_shape[2], modal_input_shape[3]);
+	// 模型输入和输出形状
+	// 批 × 长 × 宽 × 通道
+	std::vector<int64_t> modal_input_shape = session.GetInputTypeInfo(0).GetTensorTypeAndShapeInfo().GetShape();
+	std::vector<int64_t> modal_output_shape = session.GetOutputTypeInfo(0).GetTensorTypeAndShapeInfo().GetShape();
 
-    char *image_path = "img.jpg";
+	printf("-> 模型输入形态：%lld × %lld × %lld × %lld\n", modal_input_shape[0], modal_input_shape[1], modal_input_shape[2], modal_input_shape[3]);
 
-    // 读取图片
-    cv::Mat input_image = cv::imread(image_path, cv::ImreadModes::IMREAD_COLOR);
+	string image_path = "img.jpg";
 
-    printf("-> 图片大小形态：%d × %d × %d\n", input_image.rows, input_image.cols, input_image.channels());
+	// 读取图片
+	cv::Mat input_image = cv::imread(image_path.data(), cv::ImreadModes::IMREAD_COLOR);
 
-    cv::resize(input_image, input_image, cv::Size((int)modal_input_shape.at(1), (int)modal_input_shape.at(2)), cv::InterpolationFlags::INTER_CUBIC);
+	printf("-> 图片大小形态：%d × %d × %d\n", input_image.rows, input_image.cols, input_image.channels());
 
-    // OpenCV中颜色空间转换
-    cv::cvtColor(input_image, input_image, cv::ColorConversionCodes::COLOR_BGR2RGB);
+	cv::resize(input_image, input_image, cv::Size((int)modal_input_shape.at(1), (int)modal_input_shape.at(2)), cv::InterpolationFlags::INTER_CUBIC);
 
-    // 转换成浮点数，并且归一化
-    input_image.convertTo(input_image, CV_32F, 1.0 / 255.0, -1);
+	// OpenCV中颜色空间转换
+	cv::cvtColor(input_image, input_image, cv::ColorConversionCodes::COLOR_BGR2RGB);
 
-    std::vector<int64_t> input_tensor_dims = {1, input_image.rows, input_image.cols, input_image.channels()};
+	// 转换成浮点数，并且归一化
+	input_image.convertTo(input_image, CV_32F, 1.0 / 255.0, -1);
 
-    // 输入张量
-    Ort::Value input_tensor = Ort::Value::CreateTensor<float>(memory_info, input_image.ptr<float>(), input_image.total() * input_image.channels(), input_tensor_dims.data(), input_tensor_dims.size());
+	std::vector<int64_t> input_tensor_dims = { 1, input_image.rows, input_image.cols, input_image.channels() };
 
-    // 模型推理计算
-    vector<Ort::Value> output_value = session.Run(Ort::RunOptions{nullptr}, modal_input_names, &input_tensor, input_tensor_dims.at(0), modal_output_names, input_tensor_dims.at(0));
+	// 输入张量
+	Ort::Value input_tensor = Ort::Value::CreateTensor<float>(memory_info, input_image.ptr<float>(), input_image.total() * input_image.channels(), input_tensor_dims.data(), input_tensor_dims.size());
 
-    for (int i = 0; i < input_tensor_dims.at(0); i++){
-        float *out_arr = output_value[i].GetTensorMutableData<float>();
-        printf("-> 模型预测标签：%ld\n", max_element(out_arr, out_arr + modal_output_shape.at(1)) - out_arr);
-    }
+	// 模型推理计算
+	vector<Ort::Value> output_value = session.Run(Ort::RunOptions{ nullptr }, modal_input_names, &input_tensor, input_tensor_dims.at(0), modal_output_names, input_tensor_dims.at(0));
 
-    printf("-> 用时：%.3f秒\n", (double)(clock() - start_time) / CLOCKS_PER_SEC);
-    
-    return 0;
+	for (int i = 0; i < input_tensor_dims.at(0); i++) {
+		float* out_arr = output_value[i].GetTensorMutableData<float>();
+		printf("-> 模型预测标签：%ld\n", max_element(out_arr, out_arr + modal_output_shape.at(1)) - out_arr);
+	}
+
+	printf("-> 用时：%.3f秒\n", (double)(clock() - start_time) / CLOCKS_PER_SEC);
+
+	return 0;
 }
